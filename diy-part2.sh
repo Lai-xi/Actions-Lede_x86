66727a2797342194cee8d1f7b0ae557b80211486
google-you-ok? <84753324+LPDDR6-10000MHz@users.noreply.github.com>
2022-03-22 23:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 默认地址
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 切换内核
+sed -i 's/5.15/5.4/g' target/linux/rockchip/Makefile
