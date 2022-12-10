9a276a7a802106a25f1cb27eabc93c03d6df1703
Lai-xi <56962531+Lai-xi@users.noreply.github.com>
2022-12-10 21:05:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 切换内核
-sed -i 's/5.15/5.4/g' target/linux/x86/Makefile
+# sed -i 's/5.15/5.4/g' target/linux/x86/Makefile
