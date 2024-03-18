a2c47f66986e4d0b3f6c5a92e43c46f6503f8c60
Lai-xi <56962531+Lai-xi@users.noreply.github.com>
2024-03-18 17:55:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/6.1/5.15/g' target/linux/x86/Makefile
 sed -i 's/6.1/5.15/g' target/linux/rockchip/Makefile
 
-# 修改eth0为默认wan口 eth1为默认lan口
-sed -i "s/lan 'eth0'/lan 'eth1'/g" package/base-files/files/etc/board.d/99-default_network
-sed -i "s/ucidef_set_interface_wan 'eth1'/ucidef_set_interface_wan 'eth0' \&\& ucidef_set_interface_lan 'eth1' || ucidef_set_interface_lan 'eth0'/g" package/base-files/files/etc/board.d/99-default_network
 
 
 
