eebb0926f0c22ff994294b87e954a456e320ea41
骷髅头 <lasstp5011@gmail.com>
2024-07-23 17:51:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 切换内核
 sed -i 's/6.1/5.15/g' target/linux/x86/Makefile
 
-
-
-
-
+rm -rf package/feeds/helloworld/gn
