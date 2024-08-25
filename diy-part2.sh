03e1168c0478b07a81198314e74258264f8ae308
Lai-xi <56962531+Lai-xi@users.noreply.github.com>
2024-08-25 14:57:51 +0800

    Update diy-part2.sh
    更换编译内核

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,6 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 切换内核
-sed -i 's/6.6/5.15/g' target/linux/x86/Makefile
+# sed -i 's/6.6/5.15/g' target/linux/x86/Makefile
 
 # rm -rf package/feeds/helloworld/gn
