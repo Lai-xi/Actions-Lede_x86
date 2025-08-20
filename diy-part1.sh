92485cf5dcfe8653ffbbfeea83446d078cf7a504
Lai-xi <56962531+Lai-xi@users.noreply.github.com>
2025-08-20 20:20:15 +0800

    Update diy-part1.sh
    维持旧版Luci

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 启用旧版Luci
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
-sed -i '/src-git.*openwrt-23/s/^/#/' feeds.conf.default
+sed -i '/src-git.*openwrt-24/s/^/#/' feeds.conf.default
 
 
 # Add a feed source
